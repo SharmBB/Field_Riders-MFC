f863e091e41265d6b18569d9a328d6eb6540aa0d
KithusshandR <KithusshandR>
2022-03-11 16:37:27 +0530

    'Feild'

--- lib/_helpers/constants.dart
+++ lib/_helpers/constants.dart
@@ -5,3 +5,6 @@ const projectName = "Field_Riders_App";
 //Major Colors
 const primaryColor = Colors.black;
 const kPrimaryWhiteColor = Colors.white;
+const kPrimaryBlueColor = Colors.blue;
+const kPrimaryGreyColor = Colors.grey;
+
