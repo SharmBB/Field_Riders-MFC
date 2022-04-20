d4b4f92c13051d2ac328e335859798873a648413
KithusshandR <KithusshandR>
2022-04-20 16:03:54 +0530

    'v2'

--- lib/views/Calendar/Calendar.dart
+++ lib/views/Calendar/Calendar.dart
@@ -1,47 +1,43 @@
- import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+DateTime selectedDate = DateTime.now();
 
-  DateTime selectedDate = DateTime.now();
-
-  var customFormat = DateFormat('dd-MM-yyyy');
+var customFormat = DateFormat('dd-MM-yyyy');
 
 Future openDate(BuildContext context) async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-          return AlertDialog(
-            insetPadding: EdgeInsets.symmetric(horizontal: 10),
-            backgroundColor: Colors.transparent,
-            content: Container(
-              // margin: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 60),
-              height: MediaQuery.of(context).size.height * 0.4,
-              width: MediaQuery.of(context).size.width * 1,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: CalendarDatePicker(
-                
-                initialDate: DateTime.now(),
-                firstDate: DateTime(DateTime.now().year - 10),
-                lastDate: DateTime(DateTime.now().year + 10),
-                onDateChanged: (DateTime picked) async {
-                  
-                  if (picked != null && picked != selectedDate)
-                    setState(() {
-                      selectedDate = picked;
-                      print(selectedDate);
-                    });
-                  Navigator.pop(context,);
-                },
-              ),
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+        return AlertDialog(
+          insetPadding: EdgeInsets.symmetric(horizontal: 10),
+          backgroundColor: Colors.transparent,
+          content: Container(
+            // margin: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 60),
+            height: MediaQuery.of(context).size.height * 0.4,
+            width: MediaQuery.of(context).size.width * 1,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
             ),
-          );
-        });
-      },
-    );
-  }
+            child: CalendarDatePicker(
+              initialDate: DateTime.now(),
+              firstDate: DateTime(DateTime.now().year - 10),
+              lastDate: DateTime(DateTime.now().year + 10),
+              onDateChanged: (DateTime picked) async {
+                if (picked != null && picked != selectedDate)
+                  setState(() {
+                    selectedDate = picked;
+                    print(selectedDate);
+                  });
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        );
+      });
+    },
+  );
+}
